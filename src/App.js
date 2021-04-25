bd3e8532e9eaa743793858d3cab4ef8b3655de3b
Alexis Borges <tuaalexis@gmail.com>
2021-04-24 18:43:15 -0600

    Creados componentes para las actividaes y las paginas de carrito de compras

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { DataProvider } from './context/DataProvider';
+
+import Cart from './pages/cart';
+import ActivityPage from './pages/activity';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <DataProvider>
+          <div className="container">
+            <div className="row">
+              <div class="col">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                  <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                      <Link className="nav-link" to="/">Actividades</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/cart">Carrito</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <Switch>
+              <Route path="/cart">
+                <Cart />
+              </Route>
+
+              <Route path="/">
+                <ActivityPage />
+              </Route>
+
+            </Switch>
+          </div>
+
+        </DataProvider>
+      </div>
+    </Router>
   );
 }
 
